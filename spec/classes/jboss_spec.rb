8d19473e72ea0b824d5168842d24835eee1c33e2
fdammeke <fabian.dammekens@cegeka.be>
2015-08-11 11:52:35 +0200

    Fix SPEC tests

--- spec/classes/jboss_spec.rb
+++ spec/classes/jboss_spec.rb
@@ -3,6 +3,6 @@
 require 'spec_helper'
 
 describe 'jboss' do
-  let(:params) { { :version => '6.3.0-2.cgk.el6' } }
+  let(:params) { { :version => '6.3.0-2.cgk.el6', :jboss_env_props => [{ 'test' => 'value'}] } }
   it { should contain_class 'jboss' }
 end
